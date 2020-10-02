ce586b1ddb25ee96cfe03d26488e425b060bbed8
Ved Prakash <55890729+vedarya1@users.noreply.github.com>
2020-10-03 00:43:44 +0530

    Add files via upload

---
+++ WayTooLongWords.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	string s;
+	int t;
+	cin >> t;
+	while(t--) {
+		cin >> s;
+		if(s.length() > 10) {
+			cout << s[0] << s.length()-2 << s[s.length()-1] << endl;
+		}
+		else {
+			cout << s << endl;
+		}
+	}
+	return 0;
+}
